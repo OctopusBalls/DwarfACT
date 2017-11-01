5332934d8ab8aef4b0a22790f7ae931b3b4e3999
debugon <games0421@gmail.com>
2017-11-01 15:46:27 +0900

    no message

---
+++ Assets/Scripts/NeverSleep.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeverSleep : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //カメラ起動中は無操作が多いので、スリープ時間を無効に
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
