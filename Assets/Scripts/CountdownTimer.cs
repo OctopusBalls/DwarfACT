4f6ad95865ac58335f5c0becd55ee0450be080c9
debugon <games0421@gmail.com>
2017-11-01 19:35:47 +0900

    フォントを変更し、制限時間を追加

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour {
+    private float limitTime;
+    public Text sourceText;
+
+    // Use this for initialization
+    void Start()
+    {
+        limitTime = 180.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(limitTime <= 0.0f)
+        {
+            SceneManager.LoadScene("StartMenu");
+        }
+        else{
+            /*
+            int minute = (int)limitTime / 60;
+            int second = (int)limitTime % 60;
+
+            sourceText.text = minute.ToString() + ":" + second.ToString();
+            */
+
+            sourceText.text = limitTime.ToString();
+            limitTime -= Time.deltaTime;
+        }
+    }
+}
