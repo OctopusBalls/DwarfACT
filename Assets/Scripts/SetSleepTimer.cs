4ba9890a59e48db9a55fb2a6cb0163b15e5619df
debugon <games0421@gmail.com>
2017-11-08 18:10:45 +0900

    端末のスリープ設定をシーンで切り替えるようにし、Prefabに登録

---
+++ Assets/Scripts/SetSleepTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SetSleepTimer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (SceneManager.GetActiveScene().name == "MarkerTraking")
+        {
+            //カメラ起動中は無操作が多いので、スリープ時間を無効に
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+        else
+        {
+            //それ以外のシーンではシステムのデフォルト
+            Screen.sleepTimeout = SleepTimeout.SystemSetting;
+        }
+	}
+}
