99e5555cc45f56060ffbc15bf756ef3914ad5848
debugon <games0421@gmail.com>
2017-11-24 23:54:20 +0900

    アタッチしたオブジェクトの周りにドワーフのモデルをクローンするスクリプト

--- Assets/Scripts/PopoutDwarf.cs
+++ Assets/Scripts/PopoutDwarf.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class PopoutDwarf : MonoBehaviour {
 
+    public GameObject originDwarfModel;
+    private GameObject clonedDwarfModel;
+
 	// Use this for initialization
 	void Start () {
-		
+        clonedDwarfModel = Object.Instantiate(originDwarfModel) as GameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Transform myTransform = this.transform;
+
+        float popoutX = myTransform.position.x + Random.Range(-1.0f, 1.0f);
+        float popoutZ = myTransform.position.z + Random.Range(-1.0f, 1.0f);
+
+        clonedDwarfModel.transform.position = new Vector3(popoutX, myTransform.position.y, popoutZ);
+    }
 }
