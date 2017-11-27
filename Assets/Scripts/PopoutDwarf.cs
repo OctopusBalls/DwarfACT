743901100b8be312cd59ec9453ba7329db738652
debugon <games0421@gmail.com>
2017-11-27 14:42:36 +0900

    触れたオブジェクトの名前を取得する処理を追加

--- Assets/Scripts/PopoutDwarf.cs
+++ Assets/Scripts/PopoutDwarf.cs
@@ -11,14 +11,36 @@ public class PopoutDwarf : MonoBehaviour {
 	void Start () {
         clonedDwarfModel = Object.Instantiate(originDwarfModel) as GameObject;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        Transform myTransform = this.transform;
 
-        float popoutX = myTransform.position.x + Random.Range(-1.0f, 1.0f);
-        float popoutZ = myTransform.position.z + Random.Range(-1.0f, 1.0f);
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetClickObject().name == "Door")
+        {
+            Transform myTransform = this.transform;
 
-        clonedDwarfModel.transform.position = new Vector3(popoutX, myTransform.position.y, popoutZ);
+            float popoutX = myTransform.position.x + Random.Range(-1.0f, 1.0f);
+            float popoutZ = myTransform.position.z + Random.Range(-1.0f, 1.0f);
+
+            clonedDwarfModel.transform.position = new Vector3(popoutX, myTransform.position.y, popoutZ);
+        }
+    }
+
+    public GameObject GetClickObject()
+    {
+        GameObject result = null;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                result = hit.collider.gameObject;
+            }
+        }
+
+        return result;
     }
 }
