6b15a8a420250912bf980f5ca611cea263ee600e
平岡大 <babon0425@gmail.com>
2018-01-03 01:53:08 +0900

    ボタンを配置してそれを押すと作成されたメッシュが非表示になる

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+
+public class ButtonScript : MonoBehaviour{
+
+    public GameObject dynamicMesh;
+    Renderer rend;
+    Shader shader1;
+    public Material ReadyMeshMaterials;
+    public Material StartMeshMatarials;
+    private void Start()
+    {
+        dynamicMesh = GameObject.Find("DynamicMesh");
+        rend = dynamicMesh.GetComponent<Renderer>();
+        ReadyMeshMaterials = Resources.Load<Material>("ReadyMeshMaterial");
+        StartMeshMatarials = Resources.Load<Material>("MeshMaterials");
+    }
+
+    public void OnClick(){
+        Debug.Log("Button Click");
+        rend.material = StartMeshMatarials;
+    } 
+}
