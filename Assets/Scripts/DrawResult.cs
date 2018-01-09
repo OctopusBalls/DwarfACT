af92d52e674d63cee1cc775cd856af5834082e8c
debugon <games0421@gmail.com>
2018-01-10 02:19:07 +0900

    変数共有の準備と数字が加算されるような処理を追加

--- Assets/Scripts/DrawResult.cs
+++ Assets/Scripts/DrawResult.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class DrawResult : MonoBehaviour {
+    public Text timeText;
+    public Text scoreText;
+    public Text TotalText;
+
+    private int totalScore;
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void RollNumber(int max, Text OutText)
+    {
+        for(int i = 0; i < max; i++)
+        {
+            OutText.text = i.ToString();
+        }
+    }
 }
