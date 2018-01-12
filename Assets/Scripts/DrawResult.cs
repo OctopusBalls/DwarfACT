dc9730d2ded3bc72c8ff1be9cef55bc131364166
平岡大 <babon0425@gmail.com>
2018-01-13 02:10:28 +0900

    csファイルを更新

--- Assets/Scripts/DrawResult.cs
+++ Assets/Scripts/DrawResult.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -39,45 +38,3 @@ public class DrawResult : MonoBehaviour {
         }
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-
-public class DrawResult : MonoBehaviour {
-    public Text timeText;
-    public Text scoreText;
-    public Text totalText;
-    
-	// Use this for initialization
-	void Start ()
-    {
-        int nowScore = CountdownTimer.getNowTime();
-        timeText.text = nowScore.ToString("D3");
-        //RollNumber(nowScore, timeText);
-
-        int gameScore = ScoreManager.getScore();
-        scoreText.text = gameScore.ToString("D3");
-        //RollNumber(gameScore, scoreText);
-
-        int totalScore = nowScore + gameScore;
-        totalText.text = totalScore.ToString("D3");
-        //RollNumber(totallScore, totalText);
-;	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    private void RollNumber(int max, Text OutText)
-    {
-        for(int i = 0; i < max; i++)
-        {
-            OutText.text = i.ToString();
-        }
-    }
-}
->>>>>>> origin/develop
